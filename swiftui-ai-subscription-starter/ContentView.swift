3e26e9fd2f304c45a5fdb486b7566141336085cb
Eugenio Barquín <eu.barquin@me.com>
2026-03-09 10:25:46 +0100

    Initial Commit

---
+++ swiftui-ai-subscription-starter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-ai-subscription-starter
+//
+//  Created by Eugenio Barquin on 9/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-ai-subscription-starter/swiftui_ai_subscription_starterApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_ai_subscription_starterApp.swift
+//  swiftui-ai-subscription-starter
+//
+//  Created by Eugenio Barquin on 9/3/26.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_ai_subscription_starterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
